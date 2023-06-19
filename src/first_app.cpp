33320ef55b4ef4771a580c52a318ee0f072ad0fa
yxie326 <maple100099@gmail.com>
2023-06-19 15:35:35 -0700

    Tutorial 9: Push Constants

--- src/first_app.cpp
+++ src/first_app.cpp
@@ -1,10 +1,21 @@
 #include "first_app.hpp"
 
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/glm.hpp>
+
 #include <stdexcept>
+#include <iostream>
 #include <array>
 
 namespace lve
 {
+    struct SimplePushConstantData
+    {
+        glm::vec2 offset;
+        alignas(16) glm::vec3 color;
+    };
+
     FirstApp::FirstApp()
     {
         loadModels();
@@ -20,7 +31,6 @@ namespace lve
 
     void FirstApp::run()
     {
-
         while (!lveWindow.shouldClose())
         {
             glfwPollEvents();
@@ -42,14 +52,23 @@ namespace lve
 
     void FirstApp::createPipelineLayout()
     {
-        auto pipelineLayoutInfo = []()
+        auto pushConstantRange = []()
+        {
+            VkPushConstantRange range{};
+            range.stageFlags = VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT;
+            range.offset = 0;
+            range.size = sizeof(SimplePushConstantData);
+            return range;
+        }();
+
+        auto pipelineLayoutInfo = [&]()
         {
             VkPipelineLayoutCreateInfo info{};
             info.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
             info.setLayoutCount = 0;
             info.pSetLayouts = nullptr;
-            info.pushConstantRangeCount = 0;
-            info.pPushConstantRanges = nullptr;
+            info.pushConstantRangeCount = 1;
+            info.pPushConstantRanges = &pushConstantRange;
             return info;
         }();
 
@@ -131,6 +150,9 @@ namespace lve
 
     void FirstApp::recordCommandBuffer(int imageIndex)
     {
+        static int frame {0};
+        frame = (frame + 1) % 100;
+
         VkCommandBufferBeginInfo beginInfo{};
         beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
 
@@ -148,7 +170,7 @@ namespace lve
         renderPassInfo.renderArea.extent = lveSwapChain->getSwapChainExtent();
 
         std::array<VkClearValue, 2> clearValues{};
-        clearValues[0].color = {0.1f, 0.1f, 0.1f, 1.0f};
+        clearValues[0].color = {0.01f, 0.01f, 0.01f, 1.0f};
         clearValues[1].depthStencil = {1.0f, 0};
         renderPassInfo.clearValueCount = static_cast<uint32_t>(clearValues.size());
         renderPassInfo.pClearValues = clearValues.data();
@@ -168,7 +190,21 @@ namespace lve
 
         lvePipeline->bind(commandBuffers[imageIndex]);
         lveModel->bind(commandBuffers[imageIndex]);
-        lveModel->draw(commandBuffers[imageIndex]);
+
+        for (int j{0}; j < 4; ++j)
+        {
+            SimplePushConstantData push{
+                {-0.5f + frame * 0.02f, -0.4f + j * 0.25f},
+                {0.0f, 0.0f, 0.02f + 0.2f * j}};
+            vkCmdPushConstants(
+                commandBuffers[imageIndex],
+                pipelineLayout,
+                VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT,
+                0,
+                sizeof(SimplePushConstantData),
+                &push);
+            lveModel->draw(commandBuffers[imageIndex]);
+        }
 
         vkCmdEndRenderPass(commandBuffers[imageIndex]);
         if (vkEndCommandBuffer(commandBuffers[imageIndex]) != VK_SUCCESS)
