8c25888fc45730857fe003ae18d789d92ab112a0
yxie326 <maple100099@gmail.com>
2023-06-21 03:31:11 -0700

    Tutorial 17: Loading 3D models

--- inc/lve_model.hpp
+++ inc/lve_model.hpp
@@ -6,6 +6,9 @@
 
 #include "lve_device.hpp"
 
+#include <memory>
+#include <vector>
+
 namespace lve
 {
     class LveModel
@@ -13,21 +16,36 @@ namespace lve
     public:
         struct Vertex
         {
-            glm::vec3 position;
-            glm::vec3 color;
+            glm::vec3 position{};
+            glm::vec3 color{};
+            glm::vec3 normal{};
+            glm::vec2 uv{};
 
             static std::vector<VkVertexInputBindingDescription> getBindingDescriptions();
             static std::vector<VkVertexInputAttributeDescription> getAttributeDescriptions();
+
+            bool operator==(const Vertex &other) const
+            {
+                return position == other.position &&
+                       color == other.color &&
+                       normal == other.normal &&
+                       uv == other.uv;
+            }
         };
 
         struct Builder
         {
             std::vector<Vertex> vertices{};
             std::vector<uint32_t> indices{};
+
+            void loadModel(const std::string &filePath);
         };
 
         LveModel(LveDevice &lveDevice, const Builder &builder);
         ~LveModel();
+
+        static std::unique_ptr<LveModel> createModelFromFile(
+            LveDevice &device, const std::string &filePath);
 
         LveModel(const LveModel &) = delete;
         LveModel &operator=(const LveModel &) = delete;
--- src/lve_model.cpp
+++ src/lve_model.cpp
@@ -1,6 +1,28 @@
 #include "lve_model.hpp"
+#include "lve_utils.hpp"
+
+#define TINYOBJLOADER_IMPLEMENTATION
+#include <tiny_obj_loader.h>
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/gtx/hash.hpp>
 
 #include <cassert>
+#include <iostream>
+#include <unordered_map>
+
+namespace std
+{
+    template <>
+    struct hash<lve::LveModel::Vertex>
+    {
+        size_t operator()(lve::LveModel::Vertex const &vertex) const
+        {
+            size_t seed{0};
+            lve::hashCombine(seed, vertex.position, vertex.color, vertex.normal, vertex.uv);
+            return seed;
+        }
+    };
+}
 
 namespace lve
 {
@@ -21,6 +43,15 @@ namespace lve
             vkDestroyBuffer(lveDevice.device(), indexBuffer, nullptr);
             vkFreeMemory(lveDevice.device(), indexBufferMemory, nullptr);
         }
+    }
+
+    std::unique_ptr<LveModel> LveModel::createModelFromFile(
+        LveDevice &device, const std::string &filePath)
+    {
+        Builder builder{};
+        builder.loadModel(filePath);
+        std::cout << "Vertex count: " << builder.vertices.size() << "\n";
+        return std::make_unique<LveModel>(device, builder);
     }
 
     void LveModel::createVertexBuffers(const std::vector<Vertex> &vertices)
@@ -138,5 +169,79 @@ namespace lve
         attributeDescriptions[1].format = VK_FORMAT_R32G32B32_SFLOAT;
         attributeDescriptions[1].offset = offsetof(Vertex, color);
         return attributeDescriptions;
+    }
+
+    void LveModel::Builder::loadModel(const std::string &filePath)
+    {
+        tinyobj::attrib_t attrib;
+        std::vector<tinyobj::shape_t> shapes;
+        std::vector<tinyobj::material_t> materials;
+        std::string warn;
+        std::string err;
+
+        if (!tinyobj::LoadObj(&attrib, &shapes, &materials, &warn, &err, filePath.c_str()))
+        {
+            throw std::runtime_error(warn + err);
+        }
+
+        vertices.clear();
+        indices.clear();
+
+        std::unordered_map<Vertex, uint32_t> uniqueVertices{};
+
+        for (const auto &shape : shapes)
+        {
+            for (const auto &index : shape.mesh.indices)
+            {
+                Vertex vertex{};
+
+                if (index.vertex_index >= 0)
+                {
+                    vertex.position = {
+                        attrib.vertices[3 * index.vertex_index + 0],
+                        attrib.vertices[3 * index.vertex_index + 1],
+                        attrib.vertices[3 * index.vertex_index + 2],
+                    };
+
+                    auto colorIndex{3 * index.vertex_index + 2};
+                    if (colorIndex < attrib.colors.size())
+                    {
+                        vertex.color = {
+                            attrib.colors[3 * index.vertex_index + 0],
+                            attrib.colors[3 * index.vertex_index + 1],
+                            attrib.colors[3 * index.vertex_index + 2],
+                        };
+                    }
+                    else
+                    {
+                        vertex.color = {1.f, 1.f, 1.f};
+                    }
+                }
+
+                if (index.normal_index >= 0)
+                {
+                    vertex.normal = {
+                        attrib.normals[3 * index.normal_index + 0],
+                        attrib.normals[3 * index.normal_index + 1],
+                        attrib.normals[3 * index.normal_index + 2],
+                    };
+                }
+
+                if (index.texcoord_index >= 0)
+                {
+                    vertex.uv = {
+                        attrib.texcoords[2 * index.texcoord_index + 0],
+                        attrib.texcoords[2 * index.texcoord_index + 1],
+                    };
+                }
+
+                if (uniqueVertices.count(vertex) == 0) {
+                    uniqueVertices[vertex] = static_cast<uint32_t>(vertices.size());
+                    vertices.push_back(vertex);
+                }
+                indices.push_back(uniqueVertices[vertex]);
+            }
+
+        }
     }
 }
