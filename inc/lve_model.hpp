b362a390b968ce9091d508c4833c6bfbc2ad6804
yxie326 <maple100099@gmail.com>
2023-06-21 00:00:58 -0700

    Tutorial 16: Index and staging buffers

--- inc/lve_model.hpp
+++ inc/lve_model.hpp
@@ -20,7 +20,13 @@ namespace lve
             static std::vector<VkVertexInputAttributeDescription> getAttributeDescriptions();
         };
 
-        LveModel(LveDevice& lveDevice, const std::vector<Vertex> &vertices);
+        struct Builder
+        {
+            std::vector<Vertex> vertices{};
+            std::vector<uint32_t> indices{};
+        };
+
+        LveModel(LveDevice &lveDevice, const Builder &builder);
         ~LveModel();
 
         LveModel(const LveModel &) = delete;
@@ -31,10 +37,17 @@ namespace lve
 
     private:
         void createVertexBuffers(const std::vector<Vertex> &vertices);
+        void createIndexBuffers(const std::vector<uint32_t> &indices);
 
         LveDevice &lveDevice;
+
         VkBuffer vertexBuffer;
         VkDeviceMemory vertexBufferMemory;
         uint32_t vertexCount;
+
+        bool hasIndexBuffer{false};
+        VkBuffer indexBuffer;
+        VkDeviceMemory indexBufferMemory;
+        uint32_t indexCount;
     };
 }
--- src/first_app.cpp
+++ src/first_app.cpp
@@ -28,7 +28,7 @@ namespace lve
         // camera.setViewDirection(glm::vec3{0.f}, glm::vec3{.5f, 0.f, 1.f});
         camera.setViewTarget(glm::vec3{-1.f, -2.f, 0.f}, glm::vec3{0.f, 0.f, 2.5f});
 
-        auto viewerObject {LveGameObject::createGameObject()};
+        auto viewerObject{LveGameObject::createGameObject()};
         KeyboardMovementController cameraController{};
 
         auto currentTime{std::chrono::high_resolution_clock::now()};
@@ -61,64 +61,56 @@ namespace lve
     }
 
     // temporary helper function, creates a 1x1x1 cube centered at offset
+    // temporary helper function, creates a 1x1x1 cube centered at offset with an index buffer
     std::unique_ptr<LveModel> createCubeModel(LveDevice &device, glm::vec3 offset)
     {
-        std::vector<LveModel::Vertex> vertices{
-
+        LveModel::Builder modelBuilder{};
+        modelBuilder.vertices = {
             // left face (white)
             {{-.5f, -.5f, -.5f}, {.9f, .9f, .9f}},
             {{-.5f, .5f, .5f}, {.9f, .9f, .9f}},
             {{-.5f, -.5f, .5f}, {.9f, .9f, .9f}},
-            {{-.5f, -.5f, -.5f}, {.9f, .9f, .9f}},
             {{-.5f, .5f, -.5f}, {.9f, .9f, .9f}},
-            {{-.5f, .5f, .5f}, {.9f, .9f, .9f}},
 
             // right face (yellow)
             {{.5f, -.5f, -.5f}, {.8f, .8f, .1f}},
             {{.5f, .5f, .5f}, {.8f, .8f, .1f}},
             {{.5f, -.5f, .5f}, {.8f, .8f, .1f}},
-            {{.5f, -.5f, -.5f}, {.8f, .8f, .1f}},
             {{.5f, .5f, -.5f}, {.8f, .8f, .1f}},
-            {{.5f, .5f, .5f}, {.8f, .8f, .1f}},
 
             // top face (orange, remember y axis points down)
             {{-.5f, -.5f, -.5f}, {.9f, .6f, .1f}},
             {{.5f, -.5f, .5f}, {.9f, .6f, .1f}},
             {{-.5f, -.5f, .5f}, {.9f, .6f, .1f}},
-            {{-.5f, -.5f, -.5f}, {.9f, .6f, .1f}},
             {{.5f, -.5f, -.5f}, {.9f, .6f, .1f}},
-            {{.5f, -.5f, .5f}, {.9f, .6f, .1f}},
 
             // bottom face (red)
             {{-.5f, .5f, -.5f}, {.8f, .1f, .1f}},
             {{.5f, .5f, .5f}, {.8f, .1f, .1f}},
             {{-.5f, .5f, .5f}, {.8f, .1f, .1f}},
-            {{-.5f, .5f, -.5f}, {.8f, .1f, .1f}},
             {{.5f, .5f, -.5f}, {.8f, .1f, .1f}},
-            {{.5f, .5f, .5f}, {.8f, .1f, .1f}},
 
             // nose face (blue)
             {{-.5f, -.5f, 0.5f}, {.1f, .1f, .8f}},
             {{.5f, .5f, 0.5f}, {.1f, .1f, .8f}},
             {{-.5f, .5f, 0.5f}, {.1f, .1f, .8f}},
-            {{-.5f, -.5f, 0.5f}, {.1f, .1f, .8f}},
             {{.5f, -.5f, 0.5f}, {.1f, .1f, .8f}},
-            {{.5f, .5f, 0.5f}, {.1f, .1f, .8f}},
 
             // tail face (green)
             {{-.5f, -.5f, -0.5f}, {.1f, .8f, .1f}},
             {{.5f, .5f, -0.5f}, {.1f, .8f, .1f}},
             {{-.5f, .5f, -0.5f}, {.1f, .8f, .1f}},
-            {{-.5f, -.5f, -0.5f}, {.1f, .8f, .1f}},
             {{.5f, -.5f, -0.5f}, {.1f, .8f, .1f}},
-            {{.5f, .5f, -0.5f}, {.1f, .8f, .1f}},
-
         };
-        for (auto &v : vertices)
+        for (auto &v : modelBuilder.vertices)
         {
             v.position += offset;
         }
-        return std::make_unique<LveModel>(device, vertices);
+
+        modelBuilder.indices = {0, 1, 2, 0, 3, 1, 4, 5, 6, 4, 7, 5, 8, 9, 10, 8, 11, 9,
+                                12, 13, 14, 12, 15, 13, 16, 17, 18, 16, 19, 17, 20, 21, 22, 20, 23, 21};
+
+        return std::make_unique<LveModel>(device, modelBuilder);
     }
 
     void FirstApp::loadGameObjects()
